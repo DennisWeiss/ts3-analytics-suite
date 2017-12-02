7db36c721f301f935aa2335e0e8b1a728de669e1
DennisWeiss <weiss.dennis98@yahoo.de>
2017-12-02 16:02:25 +0100

    Add RelationTest

---
+++ ts3socialai/src/test/java/com/weissdennis/ai/RelationTest.java
@@ -0,0 +1,30 @@
+package com.weissdennis.ai;
+
+import com.weissdennis.database.DbLoader;
+import com.weissdennis.ts3socialai.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RelationTest {
+    @BeforeAll
+    static void prepareConfig() {
+        Configuration.loadConfig("C:/Users/weiss/IdeaProjects/ts3socialai7/config.cfg");
+    }
+
+    @Test
+    void getChannelRelation() {
+        try {
+            DbLoader dbLoader = new DbLoader(Configuration.mariaDBLocation);
+            User user1 = dbLoader.loadUser("Dennis").get(0);
+            User user2 = dbLoader.loadUser("Hallencosy").get(0);
+            User.Relation relation = user1.new Relation(user2);
+            assertEquals(0.2135922330, relation.getChannelRelation(), 0.001);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
