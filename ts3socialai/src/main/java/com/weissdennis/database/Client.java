2434047f46a62e0af64b0e1a5f7968982e45465f
DennisWeiss <weiss.dennis98@yahoo.de>
2017-12-02 19:59:28 +0100

    Geo relation functionality
    - geo relation is being set now
    - uses a function to get the distance between two points (locations) on the earth sphere

--- ts3socialai/src/main/java/com/weissdennis/database/Client.java
+++ ts3socialai/src/main/java/com/weissdennis/database/Client.java
@@ -1,139 +1,138 @@
 package com.weissdennis.database;
 
 public class Client {
-   private int id;
-   private String uniqueID;
-   private String nickname;
-   private int totalUpload;
-   private int totalDownload;
-   private String ip;
-   private String hostName;
-   private String city;
-   private String region;
-   private String country;
-   private String location;
-   private String postalCode;
-   private String associatedOrg;
+    private int id;
+    private String uniqueID;
+    private String nickname;
+    private int totalUpload;
+    private int totalDownload;
+    private String ip;
+    private String hostName;
+    private String city;
+    private String region;
+    private String country;
+    private String location;
+    private String postalCode;
+    private String associatedOrg;
 
-   public Client(int id, String uniqueID, String nickname, int totalUpload, int totalDownload, String ip,
-         String hostName, String city, String region, String country, String location, String postalCode,
-         String associatedOrg) {
-      this.id = id;
-      this.uniqueID = uniqueID;
-      this.nickname = nickname;
-      this.totalUpload = totalUpload;
-      this.totalDownload = totalDownload;
-      this.ip = ip;
-      this.hostName = hostName;
-      this.city = city;
-      this.region = region;
-      this.country = country;
-      this.location = location;
-      this.postalCode = postalCode;
-      this.associatedOrg = associatedOrg;
-   }
+    public Client(int id, String uniqueID, String nickname, int totalUpload, int totalDownload, String ip, String hostName, String city,
+            String region, String country, String location, String postalCode, String associatedOrg) {
+        this.id = id;
+        this.uniqueID = uniqueID;
+        this.nickname = nickname;
+        this.totalUpload = totalUpload;
+        this.totalDownload = totalDownload;
+        this.ip = ip;
+        this.hostName = hostName;
+        this.city = city;
+        this.region = region;
+        this.country = country;
+        this.location = location;
+        this.postalCode = postalCode;
+        this.associatedOrg = associatedOrg;
+    }
 
-   public int getId() {
-      return id;
-   }
+    public int getId() {
+        return id;
+    }
 
-   public void setId(int id) {
-      this.id = id;
-   }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-   public String getUniqueID() {
-      return uniqueID;
-   }
+    public String getUniqueID() {
+        return uniqueID;
+    }
 
-   public void setUniqueID(String uniqueID) {
-      this.uniqueID = uniqueID;
-   }
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
 
-   public String getNickname() {
-      return nickname;
-   }
+    public String getNickname() {
+        return nickname;
+    }
 
-   public void setNickname(String nickname) {
-      this.nickname = nickname;
-   }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-   public int getTotalUpload() {
-      return totalUpload;
-   }
+    public int getTotalUpload() {
+        return totalUpload;
+    }
 
-   public void setTotalUpload(int totalUpload) {
-      this.totalUpload = totalUpload;
-   }
+    public void setTotalUpload(int totalUpload) {
+        this.totalUpload = totalUpload;
+    }
 
-   public int getTotalDownload() {
-      return totalDownload;
-   }
+    public int getTotalDownload() {
+        return totalDownload;
+    }
 
-   public void setTotalDownload(int totalDownload) {
-      this.totalDownload = totalDownload;
-   }
+    public void setTotalDownload(int totalDownload) {
+        this.totalDownload = totalDownload;
+    }
 
-   public String getIp() {
-      return ip;
-   }
+    public String getIp() {
+        return ip;
+    }
 
-   public void setIp(String ip) {
-      this.ip = ip;
-   }
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
-   public String getHostName() {
-      return hostName;
-   }
+    public String getHostName() {
+        return hostName;
+    }
 
-   public void setHostName(String hostName) {
-      this.hostName = hostName;
-   }
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
 
-   public String getCity() {
-      return city;
-   }
+    public String getCity() {
+        return city;
+    }
 
-   public void setCity(String city) {
-      this.city = city;
-   }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-   public String getRegion() {
-      return region;
-   }
+    public String getRegion() {
+        return region;
+    }
 
-   public void setRegion(String region) {
-      this.region = region;
-   }
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
-   public String getCountry() {
-      return country;
-   }
+    public String getCountry() {
+        return country;
+    }
 
-   public void setCountry(String country) {
-      this.country = country;
-   }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-   public String getLocation() {
-      return location;
-   }
+    public String getLocation() {
+        return location;
+    }
 
-   public void setLocation(String location) {
-      this.location = location;
-   }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-   public String getPostalCode() {
-      return postalCode;
-   }
+    public String getPostalCode() {
+        return postalCode;
+    }
 
-   public void setPostalCode(String postalCode) {
-      this.postalCode = postalCode;
-   }
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
-   public String getAssociatedOrg() {
-      return associatedOrg;
-   }
+    public String getAssociatedOrg() {
+        return associatedOrg;
+    }
 
-   public void setAssociatedOrg(String associatedOrg) {
-      this.associatedOrg = associatedOrg;
-   }
+    public void setAssociatedOrg(String associatedOrg) {
+        this.associatedOrg = associatedOrg;
+    }
 }
