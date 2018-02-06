86354fb49e17b7108a4ead46c3328bf6d1f19efc
DennisWeiss <weiss.dennis98@yahoo.de>
2018-02-06 22:02:33 +0100

    Add front-end implementation for banned users

--- react-frontend/src/SocialGraph.js
+++ react-frontend/src/SocialGraph.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import Graph from 'react-graph-vis';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import axios from "axios/index";
+
+
+const bannedColor = '#ff4444';
+const colors = ['#00cc00', '#66ffff', '#009933', '#cc00ff','#ffff00', '#ff3399', '#ffffcc', '#ffffff'];
+var channelToColor = {};
+var channelIndex = 0;
+
 
 var options = {
     layout: {
@@ -49,16 +53,6 @@ var events = {
     }
 };
 
-/*function getGraph() {
-    axios.get('/users.json').then(res => {
-
-        for (let i = 0; i < res.data.length; i++) {
-            graph.nodes.push({id: res.data[i].uniqueId, label: res.data[i].nickname});
-        }
-        console.log(graph);
-    });
-}*/
-
 export default class SocialGraph extends React.Component {
     constructor(props) {
         super(props);
@@ -72,34 +66,88 @@ export default class SocialGraph extends React.Component {
     }
 
     componentWillMount() {
-        axios.get('http://localhost:8080/ts3/users').then(res => {
-            let graphnodes = this.state.graph.nodes.slice();
-            for (let i = 0; i < res.data.length; i++) {
-                graphnodes.push({id: res.data[i].uniqueID, label: res.data[i].nickname});
-            }
-            this.setState({
-                graph: {
-                    nodes: graphnodes,
-                    edges: this.state.graph.edges
-                }
-            });
-            console.log(this.state.graph);
-        });
+        let graphnodes = this.state.graph.nodes.slice();
+        axios.get('http://gr-esports.de:8080/ts3/users').then(res => {
+            axios.get('http://gr-esports.de:8080/ts3/currentusers').then(res2 => {
+                axios.get('http://gr-esports.de:8080/ts3/bans').then(res3 => {
+                    //console.log(res3.data);
+                    for (let i = 0; i < res.data.length; i++) {
+                        let node = {id: res.data[i].uniqueID, label: res.data[i].nickname};
+                        for (let j = 0; j < res2.data.length; j++) {
+                            if (res.data[i].uniqueID == res2.data[j].id) {
+                                if (channelToColor[res2.data[j].channel] != null) {
+                                    node.color = channelToColor[res2.data[j].channel];
+                                } else {
+                                    node.color = colors[channelIndex];
+                                    channelToColor[res2.data[j].channel] = colors[channelIndex];
+                                    channelIndex++;
+                                    if (channelIndex > colors.length) {
+                                        channelIndex = 0;
+                                    }
+                                }
+                                console.log(channelToColor);
+                                //node.color = '#009933';
+                            }
+                        }
+                        console.log(res3.data);
+                        for (let j = 0; j < res3.data.length; j++) {
+                            //console.log(res3.data[j]);
+                            if (node.id == res3.data[j]) {
+                                if (i == 45) {
+                                    console.log(res3.data[j]);
+                                }
+                                node.color = bannedColor;
+                                console.log(res.data[i].unqiueID);
+                            }
+                        }
+                        graphnodes.push(node);
+                    }
+                    console.log(this.state.graph);
 
-        axios.get('http://localhost:8080/ts3/relations').then(res => {
-            let edges = this.state.graph.edges.slice();
-            for (let i = 0; i < res.data.length; i++) {
-                if (res.data[i].channelRelation >= 0.02) {
-                    edges.push({from: res.data[i].user, to: res.data[i].otherUser, value: res.data[i].totalRelation});
-                }
-            }
-            this.setState({
-                graph: {
-                    nodes: this.state.graph.nodes,
-                    edges: edges
-                }
+                    axios.get('http://gr-esports.de:8080/ts3/relations').then(res4 => {
+                        let edges = this.state.graph.edges.slice();
+                        for (let i = 0; i < res4.data.length; i++) {
+                            if (res4.data[i].channelRelation >= 0.03 || res4.data[i].totalRelation >= 0.3) {
+                                let color1 = '#7da9ff';
+                                //let nodes = this.state.graph.nodes;
+                                let edge = {from: res4.data[i].user,
+                                    to: res4.data[i].otherUser,
+                                    value: res4.data[i].totalRelation,
+                                    color: {
+                                        color: color1,
+                                        inherit: false,
+                                        highlight: '#b200cc'
+                                    }};
+                                for (let j = 0; j < graphnodes.length; j++) {
+                                    if (res4.data[i].user == graphnodes[j].id) {
+                                        if (graphnodes[j].color != null) {
+                                            color1 = graphnodes[j].color;
+                                        }
+                                    }
+                                }
+                                for (let j = 0; j < graphnodes.length; j++) {
+                                    if (res4.data[i].otherUser == graphnodes[j].id) {
+                                        if (graphnodes[j].color != null) {
+                                            if (graphnodes[j].color == color1 && color1 != bannedColor) {
+                                                console.log(i);
+                                                edge.color.color = color1;
+                                            }
+                                        }
+                                    }
+                                }
+                                edges.push(edge);
+                            }
+                        }
+                        this.setState({
+                            graph: {
+                                nodes: graphnodes,
+                                edges: edges
+                            }
+                        });
+                        console.log(this.state.graph);
+                    });
+                });
             });
-            console.log(this.state.graph);
         });
     }
 
