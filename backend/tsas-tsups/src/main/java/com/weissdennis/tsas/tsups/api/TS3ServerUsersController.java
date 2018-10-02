9530c5a095b35fdd4758cf8d2d0c3eb59cedaf9a
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-10-02 22:55:13 +0200

    Fixed LocalDateTime parsing in rest controller

--- backend/tsas-tsups/src/main/java/com/weissdennis/tsas/tsups/api/TS3ServerUsersController.java
+++ backend/tsas-tsups/src/main/java/com/weissdennis/tsas/tsups/api/TS3ServerUsersController.java
@@ -4,6 +4,7 @@ import com.weissdennis.tsas.common.ts3users.TS3ServerUsers;
 import com.weissdennis.tsas.tsups.service.TS3ServerUsersService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,8 +29,8 @@ public class TS3ServerUsersController {
 
     @RequestMapping("/series-data")
     @ApiOperation(value = "Gets time series data of users count between given timestamps")
-    public HttpEntity<Iterable<? extends TS3ServerUsers>> getServersUsers(@RequestParam LocalDateTime from,
-                                                                          @RequestParam LocalDateTime to) {
+    public HttpEntity<Iterable<? extends TS3ServerUsers>> getServersUsers(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+                                                                          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
         return new ResponseEntity<>(ts3ServerUsersService.getServerUsers(from, to), HttpStatus.OK);
     }
 }
---
+++ react-frontend/src/Statistics.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import {Card} from 'antd'
+import ReactHighcharts from 'react-highcharts'
+import Highcharts from 'highcharts'
+import {convertToQueryString} from './helper/helper-functions'
+import moment from 'moment'
+
+
+class Statistics extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            usersSeriesData: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://gr-esports.de:8092/api/ts3/server-users/series-data' + convertToQueryString({
+            from: '2017-12-01',
+            to: moment().format('YYYY-MM-DD')
+        }))
+            .then(res => res.json())
+            .then(data => this.setState({usersSeriesData: data}))
+    }
+
+    render() {
+
+        const options = {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Users connected count'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Users online'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            },
+            series: [{
+                type: 'area',
+                name: 'Users online',
+                data: this.state.usersSeriesData
+            }]
+        }
+
+        return (
+            <div>
+                <Card>
+                    <ReactHighcharts config={options}/>
+                </Card>
+            </div>
+        )
+
+    }
+}
